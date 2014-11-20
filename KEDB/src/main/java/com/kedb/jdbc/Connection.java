6a0cc3f49354be5fc675233cbd72255605fcb584
syntel <syntel@syndlf1162.syntelorg.com>
2014-11-20 20:02:43 +0530

    first copy2

---
+++ KEDB/src/main/java/com/kedb/jdbc/Connection.java
@@ -0,0 +1,35 @@
+package com.kedb.jdbc;
+import java.sql.*;
+public class Connection
+{
+	java.sql.Connection con=null;
+public java.sql.Connection getConnection()
+{
+	
+	try
+	{
+	Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+	this.con=DriverManager.getConnection("jdbc:odbc:DSN","","");	
+	}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+		
+	}
+	return this.con;
+	
+}
+
+public String closeConnection()
+{
+	try {
+		con.close();
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		return e.toString();
+	}
+	return "closed";
+}
+	
+}
